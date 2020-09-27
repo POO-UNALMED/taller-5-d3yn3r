793858a35780d6f6e4ff026d7dc391c274657ca9
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2020-09-26 20:22:54 -0500

    ave

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,5 +1,20 @@
 package zooAnimales;
 
-public class Ave {
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class Ave extends Animal {
+		private static List<Ave> listado = new ArrayList<Ave>();
+		static int halcones;
+		static int aguilas;
+		String colorPlumas;
+		public Ave(String nomb,int edd, String habit, String gene, String colo){
+			super(nomb,edd,habit,gene);
+			colorPlumas = colo;
+			listado.add(this);
+		}
+		public Ave(){
+			listado.add(this);
+		}
 }
