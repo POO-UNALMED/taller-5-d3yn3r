a37f2ba74057ddea8728203bec65af85e1493de4
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2020-09-26 21:47:43 -0500

    pez

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,5 +1,14 @@
 package zooAnimales;
-
-public class Pez {
-
+import java.util.*;
+public class Pez extends Animal {
+	private static List<Pez> listado= new ArrayList<Pez>();
+	public static int salmones;
+	public static int bacalaos;
+	String colorEscamas;
+	int cantidadAletas;
+	public Pez(String nomb,int edd, String habit, String gene,String colo,int cant){
+		super(nomb,edd,habit,gene);
+		colorEscamas = colo; cantidadAletas=cant;
+		listado.add(this);
+	}
 }
