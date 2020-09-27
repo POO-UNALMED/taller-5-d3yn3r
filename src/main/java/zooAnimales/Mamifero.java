d7675f855a3f0c8b7b1b537669c38757816a5d1e
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2020-09-26 20:44:37 -0500

    mamifero

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,5 +1,18 @@
 package zooAnimales;
+import java.util.*;
 
-public class Mamifero {
 
+
+public class Mamifero extends Animal {
+	static List<Mamifero> listado= new ArrayList<Mamifero>();
+	public static int caballos;
+	public static int leones;
+	boolean pelaje;
+	int patas;
+	public Mamifero(String nom,int ed, String hab, String gen, boolean pel, int pat){
+		super(nom,ed,hab,gen);
+		pelaje = pel; patas=pat;
+		listado.add(this);
+	}
+	
 }
