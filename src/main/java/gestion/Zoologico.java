2875485447411e27106336dbaf23dad54e20c971
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2020-09-27 00:51:31 -0500

    final 3

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -3,23 +3,27 @@ package gestion;
 import java.util.*;
 
 public class Zoologico {
-	private String nombre;
+
 	private String ubicacion;
+	private String nombre;
 	private List<Zona> zona= new ArrayList<Zona>();
-	public Zoologico(String nomb, String ubic){
-		nombre = nomb;
-		ubicacion = ubic;
+	public Zoologico(String nomb, String ubica){
+		this.nombre=nomb;
+		this.ubicacion=ubica;
 	}
-	public Zoologico(){		
+	
+	public Zoologico() {
+		
 	}
-	public void agregarZonas(Zona zone) {
-		zona.add(zone);
+	public void agregarZonas(Zona zon) {
+		zona.add(zon);
 	}
+	
 	public int cantidadTotalAnimales() {
 		int cant=0;
 		for(int i=0; i< zona.size();i++) {
-			Zona pr= zona.get(i);
-			cant += pr.cantidadAnimales();
+			Zona obten= zona.get(i);
+			cant += obten.cantidadAnimales();
 		}
 		return cant;
 	}
@@ -41,4 +45,5 @@ public class Zoologico {
 	public void setZona(List<Zona> zonas) {
 		this.zona = zonas;
 	}
+
 }
